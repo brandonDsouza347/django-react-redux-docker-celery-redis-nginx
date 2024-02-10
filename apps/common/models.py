221b72d0a279fc4e353e49614baf77e490e7df69
brandon.dsouza <brandon.dsouza@tangoe.com>
2024-02-11 00:27:16 +0530

    Feature: Added profile model/signals/admin

--- apps/common/models.py
+++ apps/common/models.py
@@ -6,4 +6,9 @@ import uuid
 
 class TimeStampedUUIDModel(models.Model):
     pkid = models.BigAutoField(primary_key=True, editable=False)
-    id = mode
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
--- apps/users/models.py
+++ apps/users/models.py
@@ -18,7 +18,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
-    last_login = models.DateTimeField(auto_now=True)
     is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
