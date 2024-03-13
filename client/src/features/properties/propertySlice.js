f2d7a08f52d1d103a28ba1bfc38c5d9cf9a338e1
brandondsouza347 <brandondsouza347@gmail.com>
2024-03-14 02:28:33 +0530

    feat: add property slice

---
+++ client/src/features/properties/propertySlice.js
@@ -0,0 +1,53 @@
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import PropertyAPIService from "./propertyAPIService";
+
+const initialState = {
+    properties:[],
+    property:{},
+    isError:false,
+    isLoading:false,
+    isSuccess:false,
+    message:'',
+};
+
+
+//get all properties
+export const getProperties = createAsyncThunk('properties/getAll',async(_,thunkAPI)=>{
+    try{
+        return await PropertyAPIService.getProperties();
+    } catch (error){
+        const message = (error.response &&
+                        error.response.data && 
+                        error.response.data.message) || 
+                        error.message || 
+                        error.toString();
+
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+export const propertySlice = createSlice({
+    name:'property',
+    initialState,
+    reducers:{
+        reset: (state) => initialState
+    },
+    extraReducers: (builder)=>{
+        builder
+        .addCase(getProperties.pending, (state)=>{
+            state.isLoading = true
+        })
+        .addCase(getProperties.fulfilled,(state,actions)=>{
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.properties = actions.payload
+        })
+        .addCase(getProperties.rejected,(state,actions)=>{
+            state.isLoading = false;
+            state.isError = true;
+            state.message = actions.payload;
+        })
+    },
+})
+
+export const {reset} = propertySlice.actions;
+export default propertySlice.reducer
--- client/src/pages/PropertiesPage.jsx
+++ client/src/pages/PropertiesPage.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React,{ useEffect } from 'react'
 import { Col,Container,Row } from "react-bootstrap";
+import { useDispatch,useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+import {getProperties} from "../features/properties/propertySlice";
 
 const PropertiesPage = () => {
+  const {properties,isLoading,isError,message} = useSelector(
+      (state)=>state.properties
+      );
+
+  const dispatch = useDispatch();
+  
+  useEffect(()=>{
+    if (isError){
+        toast.error(message, {icon: ":("});
+    }
+    dispatch(getProperties());
+  },[dispatch,isError,message]);
+
+  if (isLoading){
+    return <Spinner/>;
+  }
+
   return (
     <>
     <Container>
